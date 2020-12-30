fc6fb0465f7763558a8fddf23d49dda6eee8d600
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2020-12-30 16:12:46 +0100

    Add Header component styling

--- components/Header.vue
+++ components/Header.vue
@@ -103,3 +103,129 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.site-menu {
+  display: flex;
+  justify-content: center;
+
+  .site-header > & {
+    height: 100%;
+  }
+}
+
+.site-navigation {
+  &__buttons {
+    display: flex;
+    height: 100%;
+    margin-bottom: 0;
+    padding-left: 0;
+  }
+
+  &__button {
+    width: rem(72px);
+    list-style: none;
+
+    @include respond-above(sm) {
+      width: rem(80px);
+    }
+
+    @include respond-above(md) {
+      width: rem(88px);
+    }
+  }
+
+  &__link {
+    position: relative;
+    display: inline-flex;
+    flex-wrap: wrap;
+    align-content: center;
+    align-items: center; // Not setting align-items causes erroneous alignment in IE11.
+    width: 100%;
+    height: 100%;
+    padding-top: rem(2px);
+    opacity: 0.4;
+    text-align: center;
+    text-decoration: none;
+    color: $body-color;
+    transition: $transition-base;
+    transition-property: opacity, color;
+
+    @include respond-above(sm) {
+      padding-top: rem(4px);
+    }
+
+    &:hover,
+    &:focus,
+    &[aria-current='page'] {
+      opacity: 1;
+    }
+
+    &:hover,
+    &:focus {
+      color: $body-color;
+    }
+
+    &:hover {
+      text-decoration: none;
+    }
+
+    &:focus {
+      outline-offset: 0;
+    }
+
+    &[aria-current='page'] {
+      color: $primary;
+    }
+
+    &::after {
+      $height: rem(1px);
+
+      content: '';
+      position: absolute;
+      top: calc(100% - #{$height});
+      left: 0;
+      right: 0;
+      display: inline-block;
+      height: $height;
+      opacity: 0;
+      background-color: currentColor;
+      transition: opacity $transition-base;
+
+      @at-root a:hover::after,
+        a[aria-current='page']::after {
+        opacity: 1;
+      }
+    }
+  }
+
+  &__icon,
+  &__title {
+    width: 100%;
+  }
+
+  &__title {
+    font: $headings-font-weight 0.625rem/ 1.15 $headings-font-family;
+
+    @include respond-above(sm) {
+      font-size: rem(11px);
+    }
+  }
+
+  &__icon {
+    margin-bottom: rem(6px);
+    height: rem(25px);
+
+    @include respond-above(sm) {
+      height: rem(30px);
+      margin-bottom: rem(8px);
+    }
+
+    img {
+      width: auto;
+      height: 100%;
+      vertical-align: baseline;
+    }
+  }
+}
+</style>
