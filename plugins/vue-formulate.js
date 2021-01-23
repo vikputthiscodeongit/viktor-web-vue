b78bf250f75a3a1eecf53868b2343b0a8b38cb48
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2021-01-24 00:45:19 +0100

    Add vue-formulate-i18n

--- plugins/vue-formulate.js
+++ plugins/vue-formulate.js
@@ -1,4 +1,7 @@
 import Vue from "vue";
 import VueFormulate from "@braid/vue-formulate";
+import { nl } from "@braid/vue-formulate-i18n";
 
-Vue.use(VueFormulate);
+Vue.use(VueFormulate, {
+  plugins: [ nl ]
+});
