8b45a3681def71971db6401798643aee7dc590d3
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2021-02-24 00:46:21 +0100

    Add (dummy) content to contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,13 +2,11 @@
   <section class="site-section">
     <div class="container">
       <div class="title">
-        <h1>
-          <!-- Title -->
-        </h1>
+        <h1>Lorem ipsum dolor sit amet</h1>
       </div>
 
       <div class="text">
-        <!-- Text -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare cursus orci in iaculis. Pellentesque aliquam, nunc eu porttitor suscipit, urna velit rhoncus metus, eget iaculis massa lorem et justo.</p>
       </div>
 
       <FormContact />
