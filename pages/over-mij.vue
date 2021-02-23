5d18c22066d710a9da25d600af48554a4cf70f92
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2021-02-24 00:46:46 +0100

    Add basic styling to over-mij.vue

--- pages/over-mij.vue
+++ pages/over-mij.vue
@@ -35,5 +35,65 @@ export default {
 
 .grid {
   grid-template-columns: repeat(12, minmax(10px, 1fr));
+
+  @include respond-above(md) {
+    grid-template-rows: rem(200px) repeat(2, auto);
+  }
+
+  @include respond-above(lg) {
+    grid-template-rows: rem(250px) repeat(2, auto);
+  }
+
+  @include respond-above(xxl) {
+    grid-template-rows: rem(300px) repeat(2, auto);
+  }
+}
+
+.media {
+  grid-column: 1 / span 12;
+
+  @include respond-below(md) {
+    margin-bottom: rem(40px);
+  }
+
+  @include respond-above(md) {
+    grid-column: 1 / span 7;
+    grid-row: 1 / span 3;
+  }
+
+  @include respond-above(lg) {
+    grid-column: 1 / span 6;
+  }
+}
+
+.title,
+.text {
+  grid-column: 1 / span 12;
+
+  @include respond-above(md) {
+    position: relative;
+    z-index: 1;
+    grid-column: 5 / span 8;
+  }
+
+  @include respond-above(lg) {
+    grid-column: 5 / span 6;
+  }
+
+  @include respond-above(xl) {
+    grid-column: 6 / span 5;
+  }
+}
+
+.title {
+  @include respond-above(md) {
+    grid-row: 2;
+  }
+}
+
+.text {
+  @include respond-above(md) {
+    grid-row: 3;
+  }
 }
 </style>
