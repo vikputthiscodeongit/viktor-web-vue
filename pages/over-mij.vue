facf0aaf988c4f677ea06c40c73dd20cf7d02ac6
vikputthiscodeongit <16013785+vikputthiscodeongit@users.noreply.github.com>
2021-03-10 01:00:14 +0100

    Add rellax to over-mij.vue

--- pages/over-mij.vue
+++ pages/over-mij.vue
@@ -2,19 +2,21 @@
   <main class="site-main" id="about">
     <section class="site-section">
       <div class="container grid">
-        <div class="media aspect-ratio aspect-ratio--square">
+        <div class="media aspect-ratio aspect-ratio--square rellax">
           <div class="content">
             <img src="https://via.placeholder.com/480x480" alt="">
           </div>
         </div>
 
-        <div class="title">
-          <h1>Lorem ipsum dolor sit amet</h1>
-        </div>
+        <div class="texts rellax" data-rellax-speed="4">
+          <div class="title">
+            <h1>Lorem ipsum dolor sit amet</h1>
+          </div>
 
-        <div class="text">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare cursus orci in iaculis. Pellentesque aliquam, nunc eu porttitor suscipit, urna velit rhoncus metus, eget iaculis massa lorem et justo. Curabitur auctor erat nec orci venenatis vulputate. Proin ac turpis mattis, sodales nisi iaculis, lobortis est. In hac habitasse platea dictumst. Mauris at tellus convallis, eleifend tortor ut, hendrerit turpis.</p>
-          <p>Suspendisse ultrices lacus nulla, at mattis urna sagittis sed. Aenean tellus mauris, mattis ut sagittis vitae, ultricies interdum turpis. Morbi tempor semper lectus. In vehicula sapien odio, quis luctus ante molestie ultricies. Donec convallis nibh quis metus faucibus elementum. Donec eu est varius, fringilla purus quis, luctus mauris. In hac habitasse platea dictumst.</p>
+          <div class="text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare cursus orci in iaculis. Pellentesque aliquam, nunc eu porttitor suscipit, urna velit rhoncus metus, eget iaculis massa lorem et justo. Curabitur auctor erat nec orci venenatis vulputate. Proin ac turpis mattis, sodales nisi iaculis, lobortis est. In hac habitasse platea dictumst. Mauris at tellus convallis, eleifend tortor ut, hendrerit turpis.</p>
+            <p>Suspendisse ultrices lacus nulla, at mattis urna sagittis sed. Aenean tellus mauris, mattis ut sagittis vitae, ultricies interdum turpis. Morbi tempor semper lectus. In vehicula sapien odio, quis luctus ante molestie ultricies. Donec convallis nibh quis metus faucibus elementum. Donec eu est varius, fringilla purus quis, luctus mauris. In hac habitasse platea dictumst.</p>
+          </div>
         </div>
       </div>
     </section>
@@ -22,6 +24,8 @@
 </template>
 
 <script>
+import Rellax from "rellax";
+
 export default {
   // nuxtI18n: {
   //   paths: {
@@ -29,6 +33,28 @@ export default {
   //     en: "/about"
   //   }
   // }
+
+  mounted() {
+    this.parallax();
+  },
+
+  methods: {
+    parallax() {
+      new Rellax();
+
+      // How it should be:
+
+      // if vw > md breakpoint
+        // const rellax = new Rellax();
+
+      // on resize
+        // if vw > md breakpoint && !rellax
+          // const rellax = new Rellax();
+
+        // if vw < md breakpoint && rellax
+          // rellax.destroy()
+    }
+  }
 };
 </script>
 
@@ -36,17 +62,10 @@ export default {
 @import "~/assets/scss/utilities/aspect-ratio";
 
 #about {
-  .grid {
+  .media,
+  .texts {
     @include respond-above(md) {
-      grid-template-rows: rem(200px) repeat(2, auto);
-    }
-
-    @include respond-above(lg) {
-      grid-template-rows: rem(250px) repeat(2, auto);
-    }
-
-    @include respond-above(xxl) {
-      grid-template-rows: rem(300px) repeat(2, auto);
+      grid-row: 1;
     }
   }
 
@@ -67,7 +86,6 @@ export default {
 
     @include respond-above(md) {
       grid-column: 1 / span 7;
-      grid-row: 1 / span 3;
     }
 
     @include respond-above(lg) {
@@ -75,30 +93,23 @@ export default {
     }
   }
 
-  .title,
-  .text {
+  .texts {
     grid-column: 1 / span 12;
 
     @include respond-above(md) {
       position: relative;
       z-index: 1;
       grid-column: 6 / span 7;
+      margin-top: rem(200px);
     }
 
     @include respond-above(lg) {
       grid-column: 6 / span 6;
+      margin-top: rem(250px)
     }
-  }
 
-  .title {
-    @include respond-above(md) {
-      grid-row: 2;
-    }
-  }
-
-  .text {
-    @include respond-above(md) {
-      grid-row: 3;
+    @include respond-above(xxl) {
+      margin-top: rem(300px);
     }
   }
 }
